a3fbce566b4e785e82360f647b2889f7ba1c3a32
Jimmerland <83875817+Jimmerland@users.noreply.github.com>
2021-07-24 14:23:15 +0900

    Update Asterisk.java

--- src/com/jimmer/jim/multi/dimentional/excercises/Asterisk.java
+++ src/com/jimmer/jim/multi/dimentional/excercises/Asterisk.java
@@ -1,13 +1,13 @@
 package com.jimmer.jim.multi.dimentional.excercises;
 
 public class Asterisk {
-	//row= 출력하는 줄수;
-	//num= 찍히는 별의 최대 개수;
-	//index= 별이 찍히기 시작하는 지점;
+	//row= row that gonna be printed;
+	//num= asterisk number in each line;
+	//index= starting point of asterisk;
 	
 	public void test1() {
 		int row=11;
-		int num=row;
+		int maxNum=row;
 		
 		for(int i=0;i<row;i++) {
 			int index = row-i;
@@ -24,7 +24,7 @@ public class Asterisk {
 	
 	public void test2() {
 		int row=11;
-		int num=row;
+		int maxNum=row;
 		
 		for(int i=0;i<row;i++) {
 			for(int j=0;j<=i;j++) {
@@ -109,22 +109,23 @@ public class Asterisk {
 					
 			System.out.println();	
 		}
-	}	
-	// 부호가 바뀌는 부분을 절대값으로 처리해보자.
+	}
+	//try out abstract process for reverse sign with Math.abs() 
+	
 	
 	public void test7() {
 		int row=13;
 		
 		for(int i=0;i<row;i++) {		
-			int index = Math.abs(row/2-i);//시작하는 지점
-			int blank = Math.abs(2*(i-row/2));//빈칸의 개수
-			int num = Math.abs(row-blank);//별의 개수 2*Math.abs(row/2-i)+1과 같음.
+			int index = Math.abs(row/2-i);//starting point of asterisk시작하는 지점
+			int blank = Math.abs(2*(i-row/2));//number of blank
+			int num = Math.abs(row-blank);//number of asterisk= 2*Math.abs(row/2-i)+1 = row-blank
 			for(int j=0;j<row;j++) {					
-					 if (index<=j&&j<index+num){
-						System.out.print("*");
-					} else {
-						System.out.print(" ");
-					}				
+				 if (index<=j&&j<index+num){
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}				
 			}					
 			System.out.println();	
 		}
